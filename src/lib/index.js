5e39c148ef62e8968dfe76ac7518c1e45de04fed
cyberowl <cybrowl@protonmail.com>
2023-06-24 02:40:26 -0700

    fix: export icon err

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,5 @@
 // basic_elements
 export {default as Button} from './components/basic_elements/Button.svelte';
-export {default as Icon} from './components/basic_elements/Icon.svelte';
 export {default as Modal} from './components/basic_elements/Modal.svelte';
 
 // navigation
