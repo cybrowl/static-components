18eaf61510ad619a606abf7fd0d64282f8981c81
cyberowl <cybrowl@protonmail.com>
2023-06-24 00:21:26 -0700

    fix: page nav export

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,3 +2,6 @@
 export {default as Button} from './components/basic_elements/Button.svelte';
 export {default as Icon} from './components/basic_elements/Icon.svelte';
 export {default as Modal} from './components/basic_elements/Modal.svelte';
+
+// navigation
+export {default as PageNavigation} from './components/navigation/PageNavigation.svelte';
