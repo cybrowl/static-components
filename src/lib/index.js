f7ba2186da34716a3640460a68d9c2da5aaa694d
cyberowl <cybrowl@protonmail.com>
2023-06-24 02:44:44 -0700

    remove: modal

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,5 @@
 // basic_elements
 export {default as Button} from './components/basic_elements/Button.svelte';
-export {default as Modal} from './components/basic_elements/Modal.svelte';
 
 // navigation
 export {default as PageNavigation} from './components/navigation/PageNavigation.svelte';
