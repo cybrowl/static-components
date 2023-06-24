79676971e51c0f698f3ec9e8dea6de862c888a35
cyberowl <cybrowl@protonmail.com>
2023-06-24 02:54:59 -0700

    fix: export upload button

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,7 @@
 // basic_elements
 export {default as Button} from './components/basic_elements/Button.svelte';
+export {default as UploadButton} from './components/basic_elements/UploadButton.svelte';
+export {default as Image} from './components/basic_elements/Image.svelte';
 
 // navigation
 export {default as PageNavigation} from './components/navigation/PageNavigation.svelte';
