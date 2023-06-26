ec5806fe2aeafdeacfe1463c8ac32d83376d0e45
cyberowl <cybrowl@protonmail.com>
2023-06-26 04:45:30 -0700

    add: json display for health

---
+++ src/lib/components/basic_elements/JsonDisplay.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let data = {}; // the JSON data
+
+	const isObject = item =>
+		typeof item === 'object' && !Array.isArray(item) && item !== null;
+
+	const isArray = item => Array.isArray(item);
+</script>
+
+{#if Object.keys(data).length}
+	<div class="display">
+		{#each Object.entries(data) as [key, value]}
+			{#if isObject(value)}
+				<div><b>{key}:</b></div>
+				<div style="margin-left: 20px">
+					<svelte:self data={value} />
+				</div>
+			{:else if isArray(value)}
+				<div><b>{key}:</b></div>
+				<ul>
+					{#each value as item (item)}
+						<li><svelte:self data={item} /></li>
+					{/each}
+				</ul>
+			{:else}
+				<div><b>{key}:</b> {value}</div>
+			{/if}
+		{/each}
+	</div>
+{:else}
+	<div>Loading...</div>
+{/if}
+
+<style lang="postcss">
+	.display {
+		@apply text-moon-grey;
+	}
+</style>
---
+++ src/stories/basic_elements/JsonDisplay.stories.svelte
@@ -0,0 +1,42 @@
+<script>
+	import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+	import JsonDisplay from '../../lib/components/basic_elements/JsonDisplay.svelte';
+</script>
+
+<Meta
+	title="Basic Elements/JsonDisplay"
+	component={JsonDisplay}
+	argTypes={{}}
+/>
+
+<Template let:args>
+	<div class="m-6">
+		<JsonDisplay {...args} on:click={args.onClick} />
+	</div>
+</Template>
+
+<Story
+	name="Default"
+	args={{
+		data: [
+			{
+				created: 1687773989435827000,
+				health: [
+					{assets_size: 2, heap_mb: 1, memory_mb: 4, cycles: 1000000000000}
+				],
+				id: 'hptcf-emaaa-aaaaa-qaawq-cai',
+				name: 'file_storage',
+				parent_name: 'FileScalingManager'
+			},
+			{
+				created: 1687773989435827000,
+				health: [
+					{assets_size: 4, heap_mb: 1, memory_mb: 4, cycles: 1000000000000}
+				],
+				id: 'hptcf-emaaa-aaaaa-qaawq-xxx',
+				name: 'file_storage',
+				parent_name: 'FileScalingManager'
+			}
+		]
+	}}
+/>
