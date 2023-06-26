55af19d2f8a1d56d380da431d87d7b4c90a012ed
cyberowl <cybrowl@protonmail.com>
2023-06-26 04:46:24 -0700

    fix: export component

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,6 +2,7 @@
 export {default as Button} from './components/basic_elements/Button.svelte';
 export {default as UploadButton} from './components/basic_elements/UploadButton.svelte';
 export {default as Image} from './components/basic_elements/Image.svelte';
+export {default as JsonDisplay} from './components/basic_elements/JsonDisplay.svelte';
 
 // navigation
 export {default as PageNavigation} from './components/navigation/PageNavigation.svelte';
